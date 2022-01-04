0703f1a07266706fa2e9101ebcd32d35345c940d
Bartender <bartender.winery@gmail.com>
2022-01-03 21:27:01 -0500

    Recoding for actual AI

--- app.py
+++ app.py
@@ -1,33 +1,21 @@
 print("Github indepentance version: AS-Iv1; https://github.com/BartenderWinery/PAC; Non-copyright/license")
 import os, json, time, library as lib
 from difflib import SequenceMatcher
+from library import memory
 path = os.path.dirname(os.path.abspath(__file__))
 current_time = time.strftime("%H:%M:%S", time.localtime())
-processing = False
+class command:
+    def rcon(self,msg,cmd):
+        pass
+def rcon(self,msg):
+    pass
 def load(): #main loading function
-    if(os.path.exists("library.json")&os.path.exists("tone.json")):
+    if(os.path.exists("library.json")&os.path.exists("memory.txt")):
         with open(path+"\library.json") as library:
             library = json.load(library)
-        while not processing: #Listens for messsages in input & reloads input for repeated use
+        while path: #Listens for messsages in input & reloads input for repeated use
             def listen():
-                def rcon(responses): #Check for commands
-                    if ':' in library.get(responses):
-                        match library.get(responses):
-                            case "I can respond to the following messages:":
-                                print(library)
-                            case "It is currently:":
-                                print(str(current_time))
-                            case "Clearing the screen:":
-                                os.system("call cls")
                 message = input(">>:")
-                if(message):
-                    new=0.8
-                    for responses in library: #Runs though list for matchs
-                        Sim=SequenceMatcher(None, message, responses).ratio()
-                        if Sim > new:
-                            new=Sim
-                            print(library.get(responses))
-                            rcon(responses)
             listen()
     else:
         print(lib.check("library"))
--- library.py
+++ library.py
@@ -1,5 +1,7 @@
 import os
 path = os.path.dirname(os.path.abspath(__file__))
+class memory:
+    pass
 def check(os): #Checks if json files exists
     match os:
         case "library":
@@ -23,3 +25,22 @@ def gen(package): #Main checker
                 return "Basic library successfully created."
             if(os.path.exists("memory.txt")):
                 return "Memory librarys successfully created."
+
+                #def rcon(responses): #Check for commands
+                #if ':' in library.get(responses):
+                #    match library.get(responses):
+                #        case "I can respond to the following messages:":
+                #            print(library)
+                #        case "It is currently:":
+                #            print(str(current_time))
+                #        case "Clearing the screen:":
+                #            os.system("call cls")
+
+                #if(message):
+                #    new=0.8
+                #    for responses in library: #Runs though list for matchs
+                #        Sim=SequenceMatcher(None, message, responses).ratio()
+                #        if Sim > new:
+                #            new=Sim
+                #            print(library.get(responses))
+                #            rcon(responses)
