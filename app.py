57fb67f348350706f76d3f71876b166f6b7a858b
Bartender <bartender.winery@gmail.com>
2022-01-17 13:03:19 -0500

    Proper recode for future programming

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import os, json, time, memory.library as lib, difflib
-path = os.path.dirname(os.path.abspath(__file__)); current_time = time.strftime("%H:%M:%S", time.localtime()); library = json.load(open(path+"\memory\library.json")); mods=[]
-def load():
+path = os.path.dirname(os.path.abspath(__file__)); current_time = time.strftime("%H:%M:%S", time.localtime()); library = json.load(open(path+"\memory\library.json"))
+class app:
+    def preload():
+        print("Github indepentance version: AS-Iv1; https://github.com/BartenderWinery/PAC; Non-copyright/license")
+    def s():
+        return input(">>: ")
+    def load():
+        while path:
+            system.process(app.s(),"responses",None)
+class system:
     def rcon(pack):
         match pack[1]:
             case "txt":
@@ -8,7 +16,7 @@ def load():
                     print(pack[0])
                     trace = input(">>:")
                     if trace:
-                        listen(trace,"traces",pack[2])
+                        system.process(trace,"traces",pack[2])
                 else:
                     print(pack[0]) #Trace isolation not yet supported
             case "cmd":
@@ -31,72 +39,69 @@ def load():
                                 con=package[1]
                                 quary=package[0]
                 return str(pack[0]).format(quary)
-    print("Github indepentance version: AS-Iv1; https://github.com/BartenderWinery/PAC; Non-copyright/license")
-    if(os.path.exists(path+"\memory\library.json")):
-        while path:
-            def listen(msg,server,iso):
-                if not msg==None:
-                    message=msg
-                else:
-                    message = input(">>:")
-                if(message):
-                    i=[True,True]
+    def process(msg,server,iso):
+        if msg:
+            passed=[True,True]
+            try:
+                con=0.8
+                for responses in library["phrases"][server]:
                     try:
-                        con=0.8
-                        for responses in library["phrases"][server]:
-                            try:
-                                package=[library["phrases"][server].get(responses).split(";"),difflib.SequenceMatcher(None, message, responses).ratio()]
-                                if package[1] > con:
-                                    con=package[1]
-                                    rcon(package[0])
-                                    break
-                            except:
-                                print("ERR; 002 - Please check if yfou added the correct arguments to ASI librarys.")
-                        else:
-                            i[0]=False
-                        for cmd in library["phrases"]["commands"]:
-                            try:
-                                package=[message.split(";"),library["phrases"]["commands"].get(cmd).split(";")]
-                                if package[0][0] == package[1][0]:
-                                    match package[0][0]:
-                                        case "run": #Remove ; feature and instead use all but first string word in future
-                                            os.system(package[0][1])
-                                            os.system("echo:")
-                                            print(package[1][1])
-                                            break
-                                        case "cls":
-                                            os.system("call cls")
-                                            print(package[1][1])
-                                            break
-                                        case "console":
-                                            print(package[1][1])
-                                            os.system("start "+path+"\\console.bat")
-                                            break
-                                        case "reboot":
-                                            os.system("py "+path+"\\app.py")
-                                        case "exit":
-                                            os.system("taskkill /f /im python.exe")
-                            except:
-                                print("ERR; 003 - CMD librarys error/Keyboard interruption.")
-                        else:
-                            i[1]=False
-                        if i[0]==False and i[1]==False:
-                            match server:
-                                case "responses":
-                                    print("...?")
-                                case "traces":
-                                    listen(None,"responses","none")
+                        package=[library["phrases"][server].get(responses).split(";"),difflib.SequenceMatcher(None, msg, responses).ratio()]
+                        if package[1]>con:
+                            con=package[1]
+                            system.rcon(package[0])
+                            break
                     except:
-                        print("Error; 001; Internal code error!")
-                
-            listen(None,"responses","none")
-    else:
-        print(lib.check("library"))
-        print(lib.gen("librarys"))
-        load()
+                        print("ERR 003; Response library error.")
+                else:
+                    passed[0]=False
+                for cmd in library["phrases"]["commands"]:
+                    try:
+                        package=[msg.split(";"),library["phrases"]["commands"].get(cmd).split(";")]
+                        if package[0][0] == package[1][0]:
+                            match package[0][0]:
+                                case "run":
+                                    os.system(package[0][1])
+                                    os.system("echo:")
+                                    print(package[1][1])
+                                    break
+                                case "cls":
+                                    os.system("call cls")
+                                    print(package[1][1])
+                                    break
+                                case "console":
+                                    print(package[1][1])
+                                    os.system("start "+path+"\\console.bat")
+                                    break
+                                case "reboot":
+                                    os.system("py "+path+"\\app.py")
+                                case "exit":
+                                    os.system("taskkill /f /im python.exe")
+                    except:
+                        print("ERR 004; CMD library error.")
+                else:
+                    passed[1]=False
+                if passed==[False,False]:
+                    match server:
+                        case "responses":
+                            if len(msg)>1:
+                                print("...?")
+                        case "traces":
+                            system.process(None,"responses","none")
+            except:
+                print("ERR 002; Internal code error.")
+try:
+    app.preload()
+except:
+    print("ERR 001; Preload failed.")
 if __name__ == "__main__":
-    try:
-        load()
-    except KeyboardInterrupt:
-        print(" Keyboard interruption")
-        os.system("taskkill /f /im python.exe")
+    if(os.path.exists(path+"\memory\library.json")):
+        try:
+            app.load()
+        except KeyboardInterrupt:
+            print(" Keyboard interruption")
+            os.system("taskkill /f /im python.exe")
+    else:
+        print(lib.check(path))
+        print(lib.gen(path))
+        app.load()
--- memory/library.py
+++ memory/library.py
@@ -1,16 +1,11 @@
-import os, app
-path = app.path
-def check(os): #Checks if json files exists
-    match os:
-        case "library":
-            if not os.path.exists("library.json"):
-                return "Library.json not found; Generating library at: "+path
-            else:
-                return "Library.json found; passing"
-def gen(package): #Main checker
-    match package:
-        case "librarys":
-            print(check("library","Creating library at: "+path))
-            os.system("echo > library.json")
-            if os.path.exists("library.json"):
-                return "Basic library successfully created."
+import os
+def check(path): #Checks if json files exists
+    if not os.path.exists("library.json"):
+        return "Library.json not found; Generating library at: "+path
+    else:
+        return "Library.json found; passing"
+def gen(path): #Main checker
+    print(check("library","Creating library at: "+path))
+    os.system("echo > library.json")
+    if os.path.exists("library.json"):
+        return "Basic library successfully created."
